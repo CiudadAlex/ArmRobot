095c3afb7212296573fd34678bb890c660400b8a
CiudadAlex <ciudad.alex@gmail.com>
2025-07-27 14:15:49 +0200

    ArmRobot

---
+++ setup.sh
@@ -0,0 +1,4 @@
+python3 -m venv --system-site-packages .venv
+source .venv/bin/activate
+
+sudo ./.venv/bin/pip3 install Arm_lib==1.0.0
---
+++ start.sh
@@ -0,0 +1,3 @@
+sudo git pull
+clear
+sudo /home/pi/.venv/bin/python3 /home/pi/ArmRobot/main.py
