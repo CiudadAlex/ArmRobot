0c25d457a79789b1802d7ae119816a59f3c0f2e2
CiudadAlex <ciudad.alex@gmail.com>
2025-07-29 00:33:02 +0200

    ArmRobot

--- managers/KnownPositionsManager.py
+++ managers/KnownPositionsManager.py
@@ -7,8 +7,8 @@ class KnownPositionsManager:
     HOME = [90, 90, 90, 90, 90, 90]
     STORAGE = [90, 180, 0, 0, 90, 180]
 
-    PICK_UP_CENTER_OPEN_CLAW = [90, 50, 60, 0, 90, 90]
-    PICK_UP_CENTER_CLOSE_CLAW = [90, 50, 60, 0, 90, 100]
+    PICK_UP_CENTER_OPEN_CLAW = [90, 38, 76, 0, 90, 90]
+    PICK_UP_CENTER_CLOSE_CLAW = [90, 38, 76, 0, 90, 100]
 
     instance = None
 
