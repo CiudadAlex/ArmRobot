61f95fcb0597d41e2ff90d191ecbe5aa48b797d7
CiudadAlex <ciudad.alex@gmail.com>
2025-07-31 01:20:56 +0200

    ArmRobot

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 # from managers.ControlPadManager import ControlPadManager
+# from sensors.ImageCapturer import ImageCapturer
 from commanders.KeyboardCommander import KeyboardCommander
 
 # control_pad_manager = ControlPadManager()
 # control_pad_manager.start_control()
+
+# image_capturer = ImageCapturer()
+# image_capturer.capture_image()
 
 
 if __name__ == '__main__':
