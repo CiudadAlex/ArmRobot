0521d6b1ec5bb1fc638f6889fa652beb4dd52ee5
CiudadAlex <ciudad.alex@gmail.com>
2025-07-31 02:29:40 +0200

    ArmRobot

--- main.py
+++ main.py
@@ -16,3 +16,10 @@ if __name__ == '__main__':
     keyboard_commander.start()
 
 
+# FIXME test combinations pick-drop
+# FIXME test camera capture image
+# FIXME test ControlPadManager
+
+
+# FIXME recognize color of cube in the center
+
