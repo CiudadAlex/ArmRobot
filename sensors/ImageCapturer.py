97251ac70af8de9265459887d66be2972652b21e
CiudadAlex <ciudad.alex@gmail.com>
2025-08-07 00:25:46 +0200

    ArmRobot

--- sensors/ImageCapturer.py
+++ sensors/ImageCapturer.py
@@ -6,7 +6,7 @@ class ImageCapturer:
     @staticmethod
     def capture_image():
 
-        capture = cv.VideoCapture(1, cv.CAP_V4L2)
+        capture = cv.VideoCapture(1)
         while capture.isOpened():
             try:
                 ret, img = capture.read()
