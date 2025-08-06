5ea3e86ef762a0a1cfbca89b929ae04cde781d2d
CiudadAlex <ciudad.alex@gmail.com>
2025-08-07 01:49:32 +0200

    ArmRobot

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 # from managers.ControlPadManager import ControlPadManager
 from commanders.KeyboardCommander import KeyboardCommander
 from servers import CommandServer
+from sensors.ImageCapturer import ImageCapturer
 
 # control_pad_manager = ControlPadManager()
 # control_pad_manager.start_control()
 
 
 if __name__ == '__main__':
+
+    print("######## Start capturing video")
+    ImageCapturer.get_instance().start_infinite_loop_capture()
 
     print("######## Start CommandServer")
     CommandServer.run_server()
@@ -17,8 +21,8 @@ if __name__ == '__main__':
 
 
 # FIXME test combinations pick-drop
-# FIXME test camera capture image
 # FIXME test ControlPadManager
+# FIXME see video in the HTML
 
 
 # FIXME recognize color of cube in the center
--- sensors/ImageCapturer.py
+++ sensors/ImageCapturer.py
@@ -1,7 +1,30 @@
 import cv2 as cv
+import threading
+import time
 
 
 class ImageCapturer:
+
+    instance = None
+
+    @staticmethod
+    def get_instance():
+        if ImageCapturer.instance is None:
+            ImageCapturer.instance = ImageCapturer()
+        return ImageCapturer.instance
+
+    def __init__(self):
+        self.thread = threading.Thread(target=self.infinite_loop_capture)
+        self.thread.daemon = True
+
+    def start_infinite_loop_capture(self):
+        self.thread.start()
+
+    def infinite_loop_capture(self):
+
+        while True:
+            self.capture_image()
+            time.sleep(0.1)
 
     @staticmethod
     def capture_image():
