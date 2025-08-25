8f65ee846f1b0869bc3ab4094fdb2e2ba105b7b9
CiudadAlex <ciudad.alex@gmail.com>
2025-08-26 01:26:34 +0200

    ArmRobot

--- main.py
+++ main.py
@@ -8,22 +8,33 @@ import sys
 # control_pad_manager = ControlPadManager()
 # control_pad_manager.start_control()
 
+MODE_DEFAULT = "default"
+MODE_MINIMAL = "minimal"
+
+
+def get_mode():
+
+    mode = MODE_DEFAULT
+    num_command_tokens = len(sys.argv)
+    print("Number of command tokens:", num_command_tokens)
+
+    if num_command_tokens == 2:
+        mode = sys.argv[1]
+        print("Mode:", mode)
+
+    return mode
+
 
 if __name__ == '__main__':
 
-    num_command_tokens = len(sys.argv)
-    print("Number of command tokens:", num_command_tokens)
-    # mode = sys.argv[1]
-    # print("Argument:", mode)
+    selected_mode = get_mode()
 
-    # if "minimal" == mode:
-    #     pass
+    if MODE_DEFAULT == selected_mode:
+        print("######## Start capturing video")
+        ImageCapturer.get_instance().start_infinite_loop_capture()
 
-    # print("######## Start capturing video")
-    # ImageCapturer.get_instance().start_infinite_loop_capture()
-
-    # print("######## Start CommandServer")
-    # CommandServer.run_server()
+        print("######## Start CommandServer")
+        CommandServer.run_server()
 
     print("######## Start KeyboardCommander")
     keyboard_commander = KeyboardCommander()
