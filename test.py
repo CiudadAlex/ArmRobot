b994c81553dd73f430c388e83825d5d88e2a7854
CiudadAlex <ciudad.alex@gmail.com>
2025-08-31 17:50:33 +0200

    ArmRobot

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 from utils.ImagePositionDiscoverer import ImagePositionDiscoverer
 from utils.ColorDeterminer import ColorDeterminer
 
+replacement = "######"
+image_path_template = f"C:/Alex/Dev/data_corpus/VideoCamera/cube{replacement}.jpg"
 
-image_path = "C:/Alex/Dev/data_corpus/VideoCamera/cube1.jpg"
-average_position = ImagePositionDiscoverer.get_central_position_of_filtered_pixels(image_path=image_path, filter_of_pixels=ColorDeterminer.is_red)
-
-print(f"average_position = {average_position}")
+for i in range(1, 4):
+    image_path = image_path_template.replace(replacement, str(i))
+    average_position = ImagePositionDiscoverer.get_central_position_of_filtered_pixels(image_path=image_path, filter_of_pixels=ColorDeterminer.is_red)
+    print(f"average_position{i} = {average_position}")
 
